dd6859e8e01eb6e1b404193637c77d733169088f
ciaratreanor <treanoci@bc.edu>
2026-03-21 22:50:56 -0400

    make run.sh executable

---
+++ web_version/run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Create virtual environment if it doesn't exist
+if [ ! -d "venv" ]; then
+    echo "Creating virtual environment..."
+    python3 -m venv venv
+fi
+
+# Activate virtual environment
+source venv/bin/activate
+
+# Install dependencies
+pip install -r requirements.txt --quiet
+
+# Run the app
+python app.py
